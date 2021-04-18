0c08ecc2f20faae460c846be5a4c9c3ecbac5958
Genebson <mauryavero@gmail.com>
2021-04-18 10:23:27 -0300

    Fix challenge 6

--- clase6/archivo.js
+++ clase6/archivo.js
@@ -1,54 +1,102 @@
-const fs = require('fs')
+import fs from 'fs';
+
+const products = [
+  {
+    title: "Teclado Thermaltake TT eSports Challenger Prime 3 LED",
+    price: 3399.13,
+    thumbnail: "https://i.ibb.co/Zf40F5d/imagen.jpg"
+  },
+  {
+    title: "Mouse HyperX Pulsefire Haste RGB 16000 DPI",
+    price: 4699.12,
+    thumbnail: "https://i.ibb.co/cCshx6J/imagen-1.jpg"
+  },
+  {
+    title: "Silla Gamer Redragon C502 Capricornus Black",
+    price: 30999.55,
+    thumbnail: "https://i.ibb.co/R6yQfXt/imagen-2.jpg"
+  }
+]
 
 class Archivo {
-  constructor(name) {
-    this.name = name
+  constructor(nombreArchivo) {
+    this.nombreArchivo = `${nombreArchivo}.txt`
   }
-  async leer() {
+
+  leer() {
     try {
-      const productos = fs.promises.readFile('./productos.txt')
-      return productos
-    } catch (error) {
-      return []
+      return new Promise((resolve, reject) => {
+        fs.readFile(`./archivos/${this.nombreArchivo}`, "utf-8", (err, data) => {
+          const lectura = [];
+          if (!data) {
+            fs.writeFileSync(`./archivos/${this.nombreArchivo}`, `${JSON.stringify(lectura)}`);
+            console.log("Como no existe el archivo, se ha creado uno en el directorio elegido ")
+            console.log(lectura)
+          } else {
+            console.log("DATOS TOMADOS DEL .TXT");
+            console.log(JSON.parse(data))
+          }
+        });
+      })
+    }
+    catch {
+      console.log(err)
     }
   }
-  async guardar(title, price, thumbnail) {
-    try {
-      let chequearSiExiste = await this.leer()
-      if (typeof chequearSiExiste == 'string') {
-        let json = JSON.parse(chequearSiExiste)
 
-        let productoNuevo = {
-          id: json.length + 1,
-          name: title,
-          price: price,
-          thumbnail: thumbnail
+  async guardar() {
+    let arrayProductos = []
+    try {
+      let id = 0;
+      for (let producto of products) {
+        let newID;
+
+        if (!producto) newID = 1;
+        else newID = id += 1;
+
+        const info = {
+          title: producto.title,
+          price: producto.price,
+          thumbnail: producto.thumbnail,
+          id: newID
         }
-        json.push(productoNuevo)
-        await fs.promises.writeFile('./productos.txt', JSON.stringify(json, null, 1))
-        console.log(`Producto: ${productoNuevo.name} agregado correctamente!`);
-      } else {
-        let archivoNuevo = [{
-          id: 1,
-          name: title,
-          price: price,
-          thumbnail: thumbnail
-        }]
-        await fs.promises.writeFile('./productos.txt', JSON.stringify(archivoNuevo, null, 1))
-        console.log(`Se creó correctamente el archivo "productos.txt" y se agregó el producto ${archivoNuevo[0].name}`);
 
+        arrayProductos = [...arrayProductos, info]
+
+        await fs.promises.writeFile(
+          `./archivos/${this.nombreArchivo}`,
+          `${JSON.stringify(arrayProductos)}\n`, (err) => {
+            if (err) return console.log(err)
+            console.log("error")
+          }
+        )
       }
-    } catch (error) {
-      console.log(error);
+      console.log("Información almacenada en .txt de forma exitosa \n")
+
+      fs.readFile(`./archivos/${this.nombreArchivo}`, "utf-8", (err, data) => {
+        console.log("Datos ingresados del .txt");
+        console.log(JSON.parse(data))
+      });
+
+    }
+    catch (error) {
+      console.log(error)
     }
   }
-  async borrar() {
-    fs.unlink('./productos.txt', error => {
-      if (error) {
-        console.log('No se pudo eliminar el archivo porque no existe');
-      } else {
-        console.log('Archivo eliminado');
-      }
-    })
+
+  borrar() {
+    fs.unlink(`./archivos/${this.nombreArchivo}`, (err) => {
+      if (err) return console.log(err);
+
+      console.log("Archivo .txt eliminado de forma completa");
+    });
   }
 }
+
+const test = new Archivo("productos")
+
+
+/* test.leer(); */
+// test.guardar();
+/* test.leer(); */
+/* test.borrar(); */
