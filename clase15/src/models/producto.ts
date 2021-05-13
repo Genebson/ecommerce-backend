d4dac155d389db748555be504393d2e55c1a17e9
Genebson <mauryavero@gmail.com>
2021-05-13 10:07:41 -0300

    Actualiza carpeta src con sus respectivos archivos

---
+++ clase15/src/models/producto.ts
@@ -0,0 +1,82 @@
+interface interfaceProducto {
+  id: string;
+  timestammp: Date;
+  nombre: string;
+  descripcion: string;
+  codigo: number;
+  foto: string;
+  precio: number;
+  stock: number;
+}
+
+class Productos {
+
+  constructor(public productos: interfaceProducto[]) { }
+
+  public prodBD() {
+    return this.productos
+  }
+
+  addProduct(prod: interfaceProducto, io: any) {
+    const { nombre, descripcion, codigo, foto, precio, stock } = prod
+    const id: string = (this.productos.length + 1).toString();
+    const timestammp: Date = new Date();
+    const producto = {
+      id,
+      timestammp,
+      nombre,
+      descripcion,
+      codigo,
+      foto,
+      precio,
+      stock,
+    }
+    io.sockets.emit('products', producto)
+
+    this.productos.push(producto)
+  }
+
+  getProducts() {
+    let productoVista: any[] = this.productos
+    if (!this.productos.length) productoVista = [{ error: "No hay productos cargados" }]
+    return productoVista;
+  }
+
+  findOneProduct(id: string) {
+    let producto: any = this.productos.find((prod: any) => prod.id === id);
+    if (!producto) producto = { error: 'Producto no encontrado' }
+    return producto
+  }
+
+  updateProduct(prod: any, idn: string) {
+    const { timestammp, nombre, descripcion, codigo, foto, precio, stock } = prod
+    const id = idn
+    const productoAct = {
+      id,
+      timestammp,
+      nombre,
+      descripcion,
+      codigo,
+      foto,
+      precio,
+      stock,
+    }
+    let producto: any = this.productos.find((prod: any) => prod.id === id);
+    if (!producto) producto = { error: 'Producto no encontrado' }
+    else {
+      this.productos.splice(this.productos.indexOf(producto), 1, productoAct)
+      producto = productoAct
+    }
+    return producto
+  }
+
+  delProduct(id: string) {
+    let producto: any = this.productos.find((prod) => prod.id === id);///VER INTERFACE///VER INTERFACE
+    if (!producto) producto = { error: 'Producto no encontrado' }
+    else this.productos.splice(this.productos.indexOf(producto), 1)
+
+    return producto
+  }
+}
+export default Productos;
+export { interfaceProducto };
